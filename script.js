8d32713e6e98ea6c3aece3b65b5ca83fc3d7a52d
Dima Kelasev <dimakelaseff@gmail.com>
2021-04-20 09:49:17 +0300

    fix project

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-let profile = document.querySelector('.profile__vector');
-let closeBtn = document.querySelector('.popup__close');
+let profile = document.querySelector('.profile__btn');
+let closeBtn = document.querySelector('.popup__Btn-сlose');
 let popUp = document.querySelector('.popup');
 let form = document.querySelector('.popup__form');
 let sendBtn = document.querySelector('.popup__btn')
 let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job')
+let jobInput = document.querySelector('.popup__name_job')
 let titleText = document.querySelector('.profile__title');
 let subtitleText = document.querySelector('.profile__subtitle');
 
 function openProfile() {
-	popUp.classList.add('open')
+	popUp.classList.add('popup_opened');
+	let name = titleText.textContent;
+	let work = subtitleText.textContent;
+
+	nameInput.value = name;
+	jobInput.value = work
 }
 
 function closeProfile() {
-	popUp.classList.remove('open')
+	popUp.classList.remove('popup_opened')
 }
 
 function FormSubmitHandle(e) {
 	e.preventDefault();
 	let name = nameInput.value;
 	let job = jobInput.value;
-	console.log(job)
 
 titleText.textContent = name;
 subtitleText.textContent = job;
