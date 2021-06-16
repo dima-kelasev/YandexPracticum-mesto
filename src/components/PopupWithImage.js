aa29514c942eb29b5043e9ddd6fcb2d6dfd56e45
Dima Kelasev <dimakelaseff@gmail.com>
2021-06-16 16:22:50 +0300

    removed all gitignored files

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,16 @@
+import {Popup} from "./Popup.js";
+
+export class PopupWithImage extends Popup {
+constructor(popupSelector) {
+  super(popupSelector);
+  this._popupImage = this._popup.querySelector('.popup__img-main');
+  this._popupTitle = this._popup.querySelector('.popup__img-title');
+}
+
+  open(link, name) {
+    this._popupImage.src = link;
+    this._popupImage.alt = name;
+    this._popupTitle.textContent = name;
+    super.open();
+  }
+}
